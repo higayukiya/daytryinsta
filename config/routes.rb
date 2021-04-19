31218465758aaaed301e537b7eb1ab0598992479
higayukiya <higa.yukiya@gmail.com>
2021-04-20 08:42:27 +0900

    profileコントローラーとview/showの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root to: "articles#show"
 
-  resources :profiles
+  resources :profile
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
