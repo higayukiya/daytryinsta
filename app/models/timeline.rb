bdc9010f7e1f6d7158ef34714a6899df631f6604
higayukiya <higa.yukiya@gmail.com>
2021-05-15 11:39:06 +0900

    timelineの投稿日追加

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -3,4 +3,8 @@ class Timeline < ApplicationRecord
     has_many_attached :album
 
     validates :title, presence: true, length: { maximum: 200 }
+
+    def created_add
+        Time.zone.now.yday
+    end
 end
