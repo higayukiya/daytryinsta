26abdc318c45ebd996577de1c7b02f49f3a851b1
higayukiya <higa.yukiya@gmail.com>
2021-07-26 23:59:15 +0900

    デプロイ準備とherokuの設定

---
+++ app.js
@@ -0,0 +1,16 @@
+{
+    "name": "blog app",
+    "description": "This is insta",
+    "keywords": [],
+    "website": "",
+    "repository": "https://github.com/higayukiya/daytryinsta",
+    "env": {},
+    "addons": [
+      "heroku-postgresql:hobby-dev"
+    ],
+    "buildpacks": [{
+      "url": "heroku/nodejs"
+    }, {
+      "url": "heroku/ruby"
+    }]
+  }
