b61a5a279f9f690ef02668ffdebf12dc62f4b7df
higayukiya <higa.yukiya@gmail.com>
2021-07-28 08:25:45 +0900

    app.jp変更デプロイのエラー

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 {
-    "name": "blog app",
+    "name": "yukiya-insta",
     "description": "This is blogapp",
     "keywords": [],
     "website": "",
     "repository": "https://github.com/higayukiya/daytryinsta",
     "env": {},
     "addons": [
-      "heroku-postgresql:hobby-dev"
+        "heroku-postgresql:hobby-dev"
     ],
     "buildpacks": [{
-      "url": "heroku/nodejs"
+        "url": "heroku/nodejs"
     }, {
-      "url": "heroku/ruby"
+        "url": "heroku/ruby"
     }]
-  }
+}
